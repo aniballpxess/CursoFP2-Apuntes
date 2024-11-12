45328520c0c296e6b4ffb6312b798202fe595aee
Aniballpxess <aniballpxess@gmail.com>
2024-11-12 21:03:32 +0100

    Lo trabajado en clase el 12/11/2024

--- ASIGNATURAS/DI/PRACTICA/Proyectos-DI/bin-oct-dec-hex-converter/js/scripts.js
+++ ASIGNATURAS/DI/PRACTICA/Proyectos-DI/bin-oct-dec-hex-converter/js/scripts.js
@@ -1,17 +1,16 @@
 var selected_input_base;
 var unselected_bases;
 var input_number;
-var selected_output_base;
 
-function getBase(selected_option) {
-    selected_input_base = selected_option.value;
+function getBase(selected_base) {
+    selected_input_base = selected_base.value;
     unselected_bases = {
         bin: "Binario",
         oct: "Octal",
         dec: "Decimal",
         hex: "Hexadecimal"
     };
-    delete unselected_bases[selected_option.value];
+    delete unselected_bases[selected_base.value];
     const radio_buttons = document.getElementsByName("input_base");
     radio_buttons.forEach((btn) => { btn.disabled = true; });
 
@@ -38,13 +37,13 @@ function getBase(selected_option) {
     document.getElementById("num_input").disabled = false;
 }
 
-function addDigit(digit_btn) {
+function addDigit(pressed_digit_btn) {
     const input_num_display = document.getElementById("num_input");
-    input_num_display.value = input_num_display.value + digit_btn.textContent;
+    input_num_display.value = input_num_display.value + pressed_digit_btn.textContent;
 }
 
 function getNumber(enter_btn) {
-    input_number = parseInt(document.getElementById("num_input").value);
+    input_number = document.getElementById("num_input").value;
     numpad_buttons = document.getElementsByName("numpad_btn");
     numpad_buttons.forEach((btn) => { btn.disabled = true; });
     document.getElementById("num_input").disabled = true;
@@ -60,4 +59,42 @@ function getNumber(enter_btn) {
         option.textContent = value;
         output_base.appendChild(option);
     });
+}
+
+function calculateNumber(selected_base) {
+    let input_base;
+    let output_base;
+    const selected_output_base = selected_base.value;
+    switch (selected_input_base) {
+        case "bin":
+            input_base = 2;
+            break;
+        case "oct":
+            input_base = 8;
+            break;
+        case "dec":
+            input_base = 10;
+            break;
+        case "hex":
+            input_base = 16;
+            break;
+    }
+    switch (selected_output_base) {
+        case "bin":
+            output_base = 2;
+            break;
+        case "oct":
+            output_base = 8;
+            break;
+        case "dec":
+            output_base = 10;
+            break;
+        case "hex":
+            output_base = 16;
+            break;
+    }
+    const aux_number = parseInt(input_number, input_base);
+    const output_number = aux_number.toString(output_base);
+    const output_num_display = document.getElementById("num_output");
+    output_num_display.value = output_number;
 }
--- ASIGNATURAS/PM/PRACTICA/Proyectos/Task_Manager/app/src/main/java/com/example/task_manager/CreateTaskActivity.java
+++ ASIGNATURAS/PM/PRACTICA/Proyectos/Task_Manager/app/src/main/java/com/example/task_manager/CreateTaskActivity.java
@@ -1,11 +1,14 @@
 package com.example.task_manager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -21,6 +24,7 @@ public class CreateTaskActivity extends AppCompatActivity {
     private EditText taskName_input;
     private RadioGroup taskType_input;
     private Button btn_createTask;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +48,27 @@ public class CreateTaskActivity extends AppCompatActivity {
 
         btn_createTask.setOnClickListener(v -> {
             taskName_value = taskName_input.getText().toString();
-            if (taskName_value.isEmpty() || taskType_value.isEmpty()) {
+            if (taskName_value.isBlank()) {
                 Toast.makeText(CreateTaskActivity.this, "Error creando archivo.", Toast.LENGTH_SHORT).show();
+                return;
             }
+            if (taskType_value == null) {
+                Toast.makeText(CreateTaskActivity.this, "Error creando archivo.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            switch (taskType_value) {
+                case "Prioritaria":
+                    intent = new Intent(CreateTaskActivity.this, PriorityTasksActivity.class);
+                    break;
+                case "A largo plazo":
+                    intent = new Intent(CreateTaskActivity.this, LongTermTasksActivity.class);
+                    break;
+            }
+            intent.putExtra("TASK_NAME", taskName_value);
+            intent.putExtra("TASK_TYPE", taskType_value);
+            startActivity(intent);
+            taskName_input.setText("");
+            taskType_input.clearCheck();
         });
     }
 }
