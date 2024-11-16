f5e1e8b68e716b89481127c769d4d73f48a7569e
aniballpxess <aniballpxess@outlook.com>
2024-11-16 19:32:02 +0100

    Avances en tareas de AD y PM

--- ASIGNATURAS/PM/PRACTICA/Proyectos/Dam_Application/app/src/main/java/com/dam_application/JsonParserActivity.java
+++ ASIGNATURAS/PM/PRACTICA/Proyectos/Dam_Application/app/src/main/java/com/dam_application/JsonParserActivity.java
@@ -32,11 +32,10 @@ public class JsonParserActivity extends AppCompatActivity {
     Button btn_leerJson;
     Button btn_mostrarJson;
     TextView tv_contenidoJson;
-    File archivoJson;
+    File ficheroJson;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_json_parser);
@@ -55,94 +54,122 @@ public class JsonParserActivity extends AppCompatActivity {
 
         storeJson(JsonParserActivity.this, "poliza.json");
 
-        btn_leerJson.setOnClickListener(v -> archivoJson = getFile(JsonParserActivity.this, "poliza.json"));
-        btn_mostrarJson.setOnClickListener(v -> showFile(JsonParserActivity.this, archivoJson));
+        btn_leerJson.setOnClickListener(v -> ficheroJson = getFile(JsonParserActivity.this, "poliza.json"));
+        btn_mostrarJson.setOnClickListener(v -> showFile(JsonParserActivity.this, ficheroJson));
     }
 
-    private void storeJson(Context context, String fileName)
-    {
-        //JSONObject persona = createJson(context);
-        //saveJson(context, fileName, persona);
-
+    private void storeJson(Context context, String fileName) {
         File file = new File(context.getFilesDir(), fileName);
         InputStream is;
         FileOutputStream fos;
         byte[] buffer = new byte[1024];
         int length;
-        try
-        {
+        try {
             is = context.getAssets().open(fileName);
             fos = new FileOutputStream(file, true);
-            while (true)
-            {
+            while (true) {
                 length = is.read(buffer);
-                if (length < 0)
-                {
+                if (length < 0) {
                     break;
                 }
                 fos.write(buffer, 0, length);
             }
             fos.flush();
-        }
-        catch (IOException e)
-        {
+            fos.close();
+            is.close();
+        } catch (IOException e) {
             Toast.makeText(context, "Error creando el JSON.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private File getFile(Context context, String fileName)
-    {
+    private File getFile(Context context, String fileName) {
         return new File(context.getFilesDir(), fileName);
     }
 
-    private void showFile(Context context, File file)
-    {
+    private void showFile(Context context, File file) {
         StringBuilder content = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(file)))
-        {
-            while (true)
-            {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while (true) {
                 String line = br.readLine();
-                if (line == null)
-                {
+                if (line == null) {
                     break;
                 }
                 content.append(line);
                 content.append(System.lineSeparator());
             }
             tv_contenidoJson.setText(content);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             Toast.makeText(context, "Error leyendo el JSON.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void saveJson(Context context, String fileName, JSONObject persona)
-    {
-        try
-        {
+//-----------------------------------  FUNCIONES  DEPRECIADAS  -----------------------------------//
+
+
+    /**
+     * <p>El proposito de esta funci&oacute;n es poder probar el antiguo funcionamiento de la actividad,
+     * antes de la inclusi&oacute;n de un fichero JSON externo en {@code assets}, el cual implicaba
+     * tener que crear el fichero JSON desde 0 dentro de la propia actividad (similar en concepto a
+     * {@code  FileActivity}).</p>
+     *
+     * <p>Para provar el funcinamiento de esta y las otras 2 funciones internas ya depreciadas
+     * ({@link #createJson(Context)} y {@link #saveJson(Context, String, JSONObject)}), solo se tiene
+     * que sustituir la llama a la funci&oacute;n {@link #saveJson(Context, String, JSONObject)} por
+     * una llamada a esta funci&aacute;n sin alterar los par&aacute;metros ya existentes (basta con
+     * cambiar el nombre de la funci&oacute;n y listo).</p>
+     *
+     * @param context  el contexto del cual se obtiene el directorio raiz de ficheros de la actividad
+     * @param fileName el nombre del fichero JSON a crear y guardar
+     */
+    private void alpha_storeJason(Context context, String fileName) {
+        JSONObject persona = createJson(context);
+        saveJson(context, fileName, persona);
+    }
+
+    /**
+     * <p>Esta funci&oacute;n se utilizar&iacute;a para guardar, en el dir raiz de ficheros de la app,
+     * el fichero JSON a mostrar y tambien a&ntilde;adir al fichero JSON el contenido correspondiente
+     * al mismo.</p>
+     *
+     * <p>Esta funci&oacute;n trabaja en tandem junto a {@link #createJson(Context)} para almacenar
+     * el JSON utilizado en la actividad.</p>
+     *
+     * <p>Para probar el funcionamiento de esta funci&oacute;n, refierase a la documentaci&oacute;n
+     * de {@link #alpha_storeJason(Context, String)}. </p>
+     *
+     * @param context  el contexto del cual se obtiene el directorio raiz de ficheros de la actividad
+     * @param fileName el nombre del fichero JSON a crear y guardar
+     * @param persona  el JSONObject con la info que contendr&aacute; el fichero JSON
+     */
+    private void saveJson(Context context, String fileName, JSONObject persona) {
+        try {
             File file = new File(context.getFilesDir(), fileName);
             FileWriter fileWriter = new FileWriter(file);
             fileWriter.write(persona.toString(2));
             fileWriter.close();
-        }
-        catch (IOException | JSONException e)
-        {
+        } catch (IOException | JSONException e) {
+            Toast.makeText(context, "Error almacenando el JSON.", Toast.LENGTH_SHORT).show();
             throw new RuntimeException(e);
         }
     }
 
     /**
-     * <p>Esta función se utilizaría junto con el código</p>
+     * <p>Esta funci&oacute;n se utilizar&iacute;a para crear la info contenida del JSON a guardar y
+     * mostrar por pantalla, similar a lo hecho en {@code FileActivity} pero sin el contenido
+     * aleatorizado.</p>
      *
-     * @return - una instancia de JSONObject con toda la información que conformará el contenido del fichero JSON
+     * <p>Esta funci&oacute;n trabaja en tandem junto a {@link #saveJson(Context, String, JSONObject)}
+     * para almacenar el JSON utilizado en la actividad.</p>
+     *
+     * <p>Para probar el funcionamiento de esta funci&oacute;n, refierase a la documentaci&oacute;n
+     * de {@link #alpha_storeJason(Context, String)}. </p>
+     *
+     * @return una instancia de JSONObject con toda la info que conformar&aacute; el contenido del
+     *         fichero JSON
      */
     @NonNull
-    private JSONObject createJson(Context context)
-    {
-        try
-        {
+    private JSONObject createJson(Context context) {
+        try {
             JSONObject direccion = new JSONObject();
             direccion.put("calle", "c/ Méjico 27");
             direccion.put("puerta", "5ºD");
@@ -157,10 +184,8 @@ public class JsonParserActivity extends AppCompatActivity {
             persona.put("direccion", direccion);
 
             return persona;
-        }
-        catch (JSONException e)
-        {
-            Toast.makeText(context, "Error creando el JSON.", Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            Toast.makeText(context, "Error creando el contenido del JSON.", Toast.LENGTH_SHORT).show();
             throw new RuntimeException(e);
         }
     }
