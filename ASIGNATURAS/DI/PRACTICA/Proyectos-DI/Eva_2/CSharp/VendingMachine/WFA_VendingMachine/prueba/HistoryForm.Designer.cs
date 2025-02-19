1a19af41c359dcb1958e1ad6d21499a9b76394a8
aniballpxess <aniballpxess@outlook.com>
2025-02-19 15:39:00 +0100

    Avances en APM y VM

---
+++ ASIGNATURAS/DI/PRACTICA/Proyectos-DI/Eva_2/CSharp/VendingMachine/WFA_VendingMachine/prueba/HistoryForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace prueba
+{
+    partial class HistoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.History_Table = new System.Windows.Forms.DataGridView();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Credit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GoCancel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Drink = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.History_Table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(448, 376);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 1;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(530, 376);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 2;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            // 
+            // History_Table
+            // 
+            this.History_Table.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Blue;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.History_Table.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.History_Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.History_Table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Date,
+            this.Time,
+            this.Credit,
+            this.GoCancel,
+            this.Drink,
+            this.Price});
+            this.History_Table.Cursor = System.Windows.Forms.Cursors.Default;
+            this.History_Table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.History_Table.Location = new System.Drawing.Point(0, 0);
+            this.History_Table.Name = "History_Table";
+            this.History_Table.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.History_Table.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.White;
+            this.History_Table.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.History_Table.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.Teal;
+            this.History_Table.Size = new System.Drawing.Size(643, 450);
+            this.History_Table.TabIndex = 4;
+            // 
+            // Date
+            // 
+            dataGridViewCellStyle2.Format = "d";
+            dataGridViewCellStyle2.NullValue = null;
+            this.Date.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            this.Date.ToolTipText = "Fecha";
+            // 
+            // Time
+            // 
+            dataGridViewCellStyle3.Format = "T";
+            dataGridViewCellStyle3.NullValue = null;
+            this.Time.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            this.Time.ReadOnly = true;
+            this.Time.ToolTipText = "Tiempo";
+            // 
+            // Credit
+            // 
+            dataGridViewCellStyle4.Format = "C2";
+            dataGridViewCellStyle4.NullValue = null;
+            this.Credit.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Credit.HeaderText = "Credit";
+            this.Credit.Name = "Credit";
+            this.Credit.ReadOnly = true;
+            this.Credit.ToolTipText = "Credito Ingresado";
+            // 
+            // GoCancel
+            // 
+            this.GoCancel.HeaderText = "Go | Cancel";
+            this.GoCancel.Name = "GoCancel";
+            this.GoCancel.ReadOnly = true;
+            this.GoCancel.ToolTipText = "Operacion";
+            // 
+            // Drink
+            // 
+            dataGridViewCellStyle5.NullValue = null;
+            this.Drink.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Drink.HeaderText = "Drink";
+            this.Drink.Name = "Drink";
+            this.Drink.ReadOnly = true;
+            this.Drink.ToolTipText = "Bebida";
+            // 
+            // Price
+            // 
+            dataGridViewCellStyle6.Format = "C2";
+            dataGridViewCellStyle6.NullValue = null;
+            this.Price.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            this.Price.ToolTipText = "Precio";
+            // 
+            // HistoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 450);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.History_Table);
+            this.Name = "HistoryForm";
+            this.Text = "HistoryForm";
+            this.Load += new System.EventHandler(this.HistoryForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.History_Table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.DataGridView History_Table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Credit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GoCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Drink;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+    }
+}
