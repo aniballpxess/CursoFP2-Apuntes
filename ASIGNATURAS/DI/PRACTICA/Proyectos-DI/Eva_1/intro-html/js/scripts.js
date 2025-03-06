e68ff2370a2034abe0af193342515e688e4d1e07
Aniballpxess <aniballpxess@gmail.com>
2025-03-06 15:12:40 +0100

    Reestructuracion del repo completa

--- ASIGNATURAS/DI/PRACTICA/Proyectos-DI/Eva_1/intro-html/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function hello() {
-    alert("Good day");
-    alert("Hola, ¿cómo estás?")
-}
-
-function showtime() {
-    var now = new Date();
-    alert(now);
-}
--- ASIGNATURAS/PM/PRACTICA/Proyectos/YATaMaP/app/src/main/java/edu/dam/pm/yatamap/views/main/team/TeamViewModel.java
+++
@@ -1,7 +0,0 @@
-package edu.dam.pm.yatamap.views.main.team;
-
-import androidx.lifecycle.ViewModel;
-
-public class TeamViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
