e880250cceca9156afd66839a083dbb111fee38b
aniballpxess <aniballpxess@outlook.com>
2025-03-19 22:06:37 +0100

    Primer intento - parte 10

---
+++ ASIGNATURAS/DI/PRACTICA/Enunciados/Eva_2/2_Web-calculator/Adder/Addition.js
@@ -0,0 +1,13 @@
+function F_Addition()
+{
+	var L_I_Addend_1;
+	var L_I_Addend_2;
+	var L_I_Result;
+
+	alert(G_S_Addend_1);
+	alert(G_S_Addend_2);
+	G_S_Result=G_S_Addend_1+G_S_Addend_2;
+	var L_I_Addend_1=parseInt(L_I_Addend_)
+	var L_I_Addend_2;
+	var L_I_Result;
+}
