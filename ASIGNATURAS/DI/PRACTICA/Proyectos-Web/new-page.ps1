35df3cd39ee9c782c883d9ce116a2143129e2392
Aniballpxess <aniballpxess@gmail.com>
2025-06-04 19:47:21 +0200

    script generador para proyectos web de DI terminado

--- ASIGNATURAS/DI/PRACTICA/Proyectos-Web/new-page.ps1
+++ ASIGNATURAS/DI/PRACTICA/Proyectos-Web/new-page.ps1
@@ -1,7 +1,8 @@
 # Script parameters
 param(
     [string]$Path = ".",
-    [string]$NewPageName = "blank-page"
+    [string]$NewPageName = "blank-page",
+    [switch]$Quiet
 )
 
 # Script variables
@@ -30,6 +31,15 @@ $HtmlFilePath = Join-Path -Path $NewPageDirPath -ChildPath $HtmlFileName
 $CssFilePath = Join-Path -Path $NewPageDirPath -ChildPath $CssFileName
 $JsFilePath = Join-Path -Path $NewPageDirPath -ChildPath $JsFileName
 
+# Check if the new page already exists
+if (Test-Path -Path $NewPageDirPath) {
+    Write-Host "La página " -NoNewLine
+    Write-Host -ForegroundColor Red $NewPageName -NoNewLine
+    Write-Host " ya existe en: "
+    Resolve-Path $Path | Write-Host -ForegroundColor Blue
+    exit 1
+}
+
 # Create the new page directory and files
 New-Item -Path $NewPageDirPath -ItemType Directory | Out-Null
 New-Item -Path $HtmlFilePath -ItemType File | Out-Null
@@ -38,3 +48,14 @@ New-Item -Path $JsFilePath -ItemType File | Out-Null
 
 # connect the files via HTML
 Set-Content -Path $HtmlFilePath -Value $HtmlFileContent
+
+# Check for quiet mode
+if ($Quiet.IsPresent) {
+    exit 0
+}
+
+# Script success notification
+Write-Host "Pagina con nombre " -NoNewLine
+Write-Host $NewPageName -ForegroundColor Green -NoNewLine
+Write-Host " creada en: "
+Resolve-Path $NewPageDirPath | Write-Host -ForegroundColor Blue
--- ASIGNATURAS/DI/PRACTICA/Proyectos-Web/new-project.ps1
+++ ASIGNATURAS/DI/PRACTICA/Proyectos-Web/new-project.ps1
@@ -14,10 +14,14 @@ $DefaultFirstPageName = "blank-page"
 # --- Script body ---
 # Ask user for the project's & the first page's names
 if (-not $ProjectName) {
-  $ProjectName = Read-Host "Nombre del proyecto (por defecto: $DefaultProjectName)"
+  Write-Host "Nombre del proyecto (por defecto: " -NoNewLine
+  Write-Host -ForegroundColor Cyan $DefaultProjectName -NoNewLine
+  $ProjectName = Read-Host ")"
 }
 if (-not $FirstPageName) {
-  $FirstPageName = Read-Host "Nombre de la primera pagina (por defecto: $DefaultFirstPageName)"
+  Write-Host "Nombre de la página inicial (por defecto: " -NoNewLine
+  Write-Host -ForegroundColor Cyan $DefaultFirstPageName -NoNewLine
+  $FirstPageName = Read-Host ")"
 }
 
 # Stablish default names if needed
@@ -33,12 +37,27 @@ $RootDirPath = Join-Path -Path $Path -ChildPath $ProjectName
 $SourceDirPath = Join-Path -Path $RootDirPath -ChildPath $SourceDirName
 $ResourcesDirPath = Join-Path -Path $RootDirPath -ChildPath $ResourcesDirName
 
-# Make project structure
+# Check if the project already exists
+if (Test-Path -Path $RootDirPath) {
+    Write-Host "El proyecto " -NoNewLine
+    Write-Host -ForegroundColor Red $ProjectName -NoNewLine
+    Write-Host " ya existe en: "
+    Resolve-Path $Path | Write-Host -ForegroundColor Blue
+    exit 1
+}
+
+# Create the project structure
 New-Item -Path $RootDirPath -ItemType Directory | Out-Null
 New-Item -Path $ResourcesDirPath -ItemType Directory | Out-Null
 New-Item -Path $SourceDirPath -ItemType Directory | Out-Null
-Write-Host "Estructura del proyecto creada en: $RootDirPath"
+Write-Host "Estructura del proyecto creada."
 
 # Create the first page
-& .\new-page.ps1 -Path $SourceDirPath -NewPageName $FirstPageName
-Write-Host "Pagina con nombre $FirstPageName creada"
+& .\new-page.ps1 -Path $SourceDirPath -NewPageName $FirstPageName -Quiet
+Write-Host "Pagina inicial creada."
+
+# Script success notification
+Write-Host "Creacion de proyecto " -NoNewLine
+Write-Host -ForegroundColor Green $ProjectName -NoNewLine
+Write-Host " completada en: "
+Resolve-Path $Path | Write-Host -ForegroundColor Blue
